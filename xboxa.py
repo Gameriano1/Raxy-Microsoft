e3f578f8bf653e5de2a4239b94a76fc4743e529d
Gameriano1 <75039467+Gameriano1@users.noreply.github.com>
2023-03-08 17:25:50 -0300

    Update xboxa.py

--- xboxa.py
+++ xboxa.py
@@ -1,15 +1,19 @@
 import requests as r
+import threading
+import time
 
 
-def conquista(xuid, auth):
-
+def conquistar(xuid, auth, ide, scid):
     payload = {
-        "titles": [{"expiration": 600, "id": 2013672301, "state": "active", "sandbox": "RETAIL"}]
+        "titles": [{"expiration": 600, "id": ide, "state": "active", "sandbox": "RETAIL"}]
     }
 
     payload2 = {
-        "action": "progressUpdate", "serviceConfigId": "00000000-0000-0000-0000-00007806336d", "titleId": 2013672301,
-        "userId": xuid, "achievements": [{"id": 1, "percentComplete": 100}, {"id": 2, "percentComplete": 100}, {"id": 3, "percentComplete": 100}, {"id": 4, "percentComplete": 100}, {"id": 5, "percentComplete": 100}, {"id": 6, "percentComplete": 100}, {"id": 7, "percentComplete": 100}, {"id": 8, "percentComplete": 100}]}
+        "action": "progressUpdate", "serviceConfigId": scid, "titleId": ide,
+        "userId": xuid, "achievements": [{"id": 1, "percentComplete": 100}, {"id": 2, "percentComplete": 100},
+                                         {"id": 3, "percentComplete": 100}, {"id": 4, "percentComplete": 100},
+                                         {"id": 5, "percentComplete": 100}, {"id": 6, "percentComplete": 100},
+                                         {"id": 7, "percentComplete": 100}, {"id": 8, "percentComplete": 100}]}
 
     headers1 = {
         'Accept-Encoding': 'gzip, deflate',
@@ -31,17 +35,42 @@ def conquista(xuid, auth):
         'Connection': None,
     }
 
-    response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current", json=payload, headers=headers1, verify=False)
+    response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current", json=payload,
+                      headers=headers1, verify=False)
     if response.status_code != 200:
-        response.raise_for_status()
+        p = 1
+        while response.status_code != 200:
+            print("Um Erro foi Encontrado, Esperando " + str(p) + " segundos e tentando executar novamente")
+            time.sleep(p)
+            response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current",
+                              json=payload,
+                              headers=headers1, verify=False)
+            p = p+1
 
-    responsi = r.post(f"https://achievements.xboxlive.com/users/xuid(" + xuid + ")/achievements/00000000-0000-0000-0000-00007806336d/update?", json=payload2, headers=headers2, verify=False)
+
+
+    responsi = r.post(f"https://achievements.xboxlive.com/users/xuid(" + xuid + ")/achievements/" + scid + "/update?",
+                      json=payload2, headers=headers2, verify=False)
     if responsi.status_code == 200 or responsi.status_code == 304:
-        print("Conquista conquistada com sucesso! 🤩")
-        print("-------------------------------------")
+        pass
     else:
         print(f"Algo deu errado, {responsi.status_code} ❌")
 
+
+def conquista(xuid, auth):
+    print("Fazendo as Tasks do xbox... 🙃")
+    ids = [2013672301, 1967883584, 1884090207]
+    scid = ["00000000-0000-0000-0000-00007806336d", "00000000-0000-0000-0000-0000754b8540", "00000000-0000-0000-0000-0000704cef5f"]
+    threads = []
+    for i, s in zip(ids, scid):
+        t = threading.Thread(target=conquistar, args=(xuid, auth, i, s))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+    print("Conquista conquistada com sucesso! 🤩")
+    print("-------------------------------------")
 
 
 if __name__ == '__main__':
