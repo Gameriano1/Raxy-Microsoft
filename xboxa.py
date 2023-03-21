4591a28c31c186e913137a4f980e44f55e9a0b7a
Gameriano1 <75039467+Gameriano1@users.noreply.github.com>
2023-03-21 14:16:19 -0300

    Update xboxa.py

--- xboxa.py
+++ xboxa.py
@@ -3,10 +3,18 @@ import threading
 import time
 from sys import stdout
 
-def ativar(xuid,auth,ide):
+
+def conquistar(xuid, auth, ide, scid):
     payload = {
         "titles": [{"expiration": 600, "id": ide, "state": "active", "sandbox": "RETAIL"}]
     }
+
+    payload2 = {
+        "action": "progressUpdate", "serviceConfigId": scid, "titleId": ide,
+        "userId": xuid, "achievements": [{"id": 1, "percentComplete": 100}, {"id": 2, "percentComplete": 100},
+                                         {"id": 3, "percentComplete": 100}, {"id": 4, "percentComplete": 100},
+                                         {"id": 5, "percentComplete": 100}, {"id": 6, "percentComplete": 100},
+                                         {"id": 7, "percentComplete": 100}, {"id": 8, "percentComplete": 100}]}
 
     headers1 = {
         'Accept-Encoding': 'gzip, deflate',
@@ -14,27 +22,6 @@ def ativar(xuid,auth,ide):
         'Authorization': auth,
         'Cache-Control': 'no-cache'
     }
-
-    response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current", json=payload,
-                      headers=headers1, verify=False)
-    if response.status_code != 200:
-        p = 16
-        while response.status_code != 200:
-            stdout.write(
-                "\r" + "Um Erro foi Encontrado, Esperando " + str(p) + " segundos e tentando executar novamente")
-            stdout.flush()
-            time.sleep(p)
-            response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current",
-                              json=payload,
-                              headers=headers1, verify=False)
-
-
-
-def conquistar(xuid ,auth ,ide ,scid ,v):
-
-    payload2 = {
-        "action": "progressUpdate", "serviceConfigId": scid, "titleId": ide,
-        "userId": xuid, "achievements": [{"id": v, "percentComplete": 100}]}
 
     headers2 = {
         'Accept-Encoding': 'gzip, deflate',
@@ -49,40 +36,38 @@ def conquistar(xuid ,auth ,ide ,scid ,v):
         'Connection': None,
     }
 
+    response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current", json=payload,
+                      headers=headers1, verify=False)
+    if response.status_code != 200:
+        p = 16
+        while response.status_code != 200:
+            stdout.write("\r" + "Um Erro foi Encontrado, Esperando " + str(p) + " segundos e tentando executar novamente")
+            stdout.flush()
+            time.sleep(p)
+            response = r.post(f"https://presence-heartbeat.xboxlive.com/users/xuid({xuid})/devices/current",
+                              json=payload,
+                              headers=headers1, verify=False)
+
+
+
     responsi = r.post(f"https://achievements.xboxlive.com/users/xuid(" + xuid + ")/achievements/" + scid + "/update?",
                       json=payload2, headers=headers2, verify=False)
     if responsi.status_code == 200 or responsi.status_code == 304:
         pass
-    elif responsi.status_code == 429:
-        while responsi.status_code == 429:
-            stdout.write(
-                "\r" + "Um Erro foi Encontrado, Esperando " + "30" + " segundos e tentando executar novamente")
-            stdout.flush()
-            time.sleep(30)
-            responsi = r.post(
-                f"https://achievements.xboxlive.com/users/xuid(" + xuid + ")/achievements/" + scid + "/update?",
-                json=payload2, headers=headers2, verify=False)
     else:
-        pass
+        print(f"Algo deu errado, {responsi.status_code} ❌")
+
 
 def conquista(xuid, auth):
-    ids = 2013672301
-    scid = "00000000-0000-0000-0000-00007806336d"
-    threads1 = []
-    threads2 = []
-    t = threading.Thread(target=ativar, args=(xuid, auth, ids))
-    threads2.append(t)
-    t.start()
-
-    for t in threads2:
-        t.join()
-
-    for v in range(1,25):
-        t = threading.Thread(target=conquistar, args=(xuid, auth, ids, scid, v))
-        threads1.append(t)
+    ids = [2013672301, 1967883584, 1884090207]
+    scid = ["00000000-0000-0000-0000-00007806336d", "00000000-0000-0000-0000-0000754b8540", "00000000-0000-0000-0000-0000704cef5f"]
+    threads = []
+    for i, s in zip(ids, scid):
+        t = threading.Thread(target=conquistar, args=(xuid, auth, i, s))
+        threads.append(t)
         t.start()
 
-    for t in threads1:
+    for t in threads:
         t.join()
 
 
