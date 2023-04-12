7cd9ae378e5169e36e173b33e6389d2b714bb0ff
Gameriano1 <75039467+Gameriano1@users.noreply.github.com>
2023-04-12 19:59:43 -0300

    Update taskxbox.py

--- taskxbox.py
+++ taskxbox.py
@@ -3,25 +3,18 @@ import urllib3
 from sys import stdout
 import threading
 
-def TaskXbox(authenticate,countries, cc, j, cookies):
+def TaskXbox(o, authenticate,countries, cc, j, cookies):
 
     urllib3.disable_warnings()
-
-    offers = ["_Welcome_Tour_XboxApp_Offer","_xboxapp_punchcard_RewardsOnboarding_pcparent","_xboxapp_punchcard_RewardsOnboarding_pcchild1_dset","_xboxapp_punchcard_RewardsOnboarding_pcchild3_shope","_xboxapp_punchcard_RewardsOnboarding_pcchild5_gpquest","_xboxapp_punchcard_RewardsOnboarding_pcchild6_redeem","_xboxapp_punchcard_RewardsOnboarding_pcchild7_app"]
-    if cc == "US" or cc == "BR":
-        for i in range(1, 6):
-            offers.append(f"_xboxapp_pcchild{i}_urlreward_adhocpc_FH5RallyAdventure")
 
     if authenticate == None:
         raise Exception('Coloque um authenticate valido')
 
-    for o in offers:
+    payload = {
+            "id":"","timestamp":"","type":80,"amount":1,"country":f"{cc}","retry_in_background":'true',"attributes":{"offerid":f"{countries}{o}"}
+    }
 
-        payload = {
-                "id":"","timestamp":"","type":80,"amount":1,"country":f"{cc}","retry_in_background":'true',"attributes":{"offerid":f"{countries}{o}"}
-        }
-
-        headers = {
+    headers = {
 
             'Cache-Control': 'no-cache',
             'Accept': 'application/json',
@@ -33,29 +26,36 @@ def TaskXbox(authenticate,countries, cc, j, cookies):
             'Authorization': f'{authenticate}',
             'Connection': 'Keep-Alive',
             'Host': 'prod.rewardsplatform.microsoft.com',
-        }
+    }
 
-        cu = {
+    cu = {
             'Cookie': cookies
-        }
+    }
 
-        tentar = requests.post('https://prod.rewardsplatform.microsoft.com/dapi/me/activities',json=payload, headers=headers,cookies=cu, verify=False)
-        while tentar.status_code != 200:
-            stdout.write(f"\rOcorreu um Erro 💀 , pais: {cc}")
-            tentar = requests.post('https://prod.rewardsplatform.microsoft.com/dapi/me/activities', json=payload,headers=headers, cookies=cu, verify=False)
-            if tentar.status_code == 200:
-                stdout.write("\rErro Resolvido 🔥")
+    tentar = requests.post('https://prod.rewardsplatform.microsoft.com/dapi/me/activities',json=payload, headers=headers,cookies=cu, verify=False)
+    while tentar.status_code != 200:
+        stdout.write(f"\rOcorreu um Erro 💀 , pais: {cc}")
+        tentar = requests.post('https://prod.rewardsplatform.microsoft.com/dapi/me/activities', json=payload,headers=headers, cookies=cu, verify=False)
+        if tentar.status_code == 200:
+            stdout.write("\rErro Resolvido 🔥")
 
 
 def RewardsRun(auths, je, ye):
     threads = []
-    countries = ['ENUS', 'PTBR', 'ENNZ']
-    cc = ['US', 'BR', 'NZ']
+    offers = ["_Welcome_Tour_XboxApp_Offer", "_xboxapp_punchcard_RewardsOnboarding_pcparent",
+              "_xboxapp_punchcard_RewardsOnboarding_pcchild1_dset",
+              "_xboxapp_punchcard_RewardsOnboarding_pcchild3_shope",
+              "_xboxapp_punchcard_RewardsOnboarding_pcchild5_gpquest",
+              "_xboxapp_punchcard_RewardsOnboarding_pcchild6_redeem",
+              "_xboxapp_punchcard_RewardsOnboarding_pcchild7_app"]
+    countries = ['DEDE', 'PTBR', 'ENNZ']
+    cc = ['DE', 'BR', 'NZ']
     for c, ccc in zip(countries, cc):
-        t = threading.Thread(target=TaskXbox, args=(auths, c, ccc, je, ye))
-        threads.append(t)
+        for o in offers:
+            t = threading.Thread(target=TaskXbox, args=(o, auths, c, ccc, je, ye))
+            threads.append(t)
+    for t in threads:
         t.start()
-
     for t in threads:
         t.join()
 
